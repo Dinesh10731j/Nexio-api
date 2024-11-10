c9f8136d797e68c6aadd7b48140092af19d3ba28
Dinesh10731j <dinesh1122@gmail.com>
2024-11-10 09:56:24 +0545

    view single book endpoint completed

--- blog/blog.routes.ts
+++ blog/blog.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createBlog,Blogs } from "./blog.controller";
+import { createBlog,Blogs,singleBlog } from "./blog.controller";
 import { authenticateUser } from "../middleware/authMiddleware";
 const blogRouter = Router();
 blogRouter.post("/blog/create-post",authenticateUser,createBlog);
 blogRouter.get("/blogs",Blogs);
+blogRouter.get('/:blogId',singleBlog);
 
 export default blogRouter;
